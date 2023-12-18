6b1659340753e9c4aa3204235790115da329a32f
francismiko <1485059943@qq.com>
2023-12-18 22:29:02 +0800

    feat: biome integrate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,4 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: ['@element-plus/nuxt'],
-})
+	devtools: { enabled: true },
+	modules: ["@element-plus/nuxt"],
+});
