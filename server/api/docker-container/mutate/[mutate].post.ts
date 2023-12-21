6b5704e589ef77420f4550bca509d00f2eea09bd
francismiko <1485059943@qq.com>
2023-12-21 13:43:21 +0800

    fix: lint check

--- server/api/docker-container/mutate/[mutate].post.ts
+++ server/api/docker-container/mutate/[mutate].post.ts
@@ -1,19 +1,33 @@
 import Docker from "dockerode";
 
-export type DockerContainerMutations = "start" | "pause" | "unpause" | "stop"
+export type DockerContainerMutations = "start" | "pause" | "unpause" | "stop";
 
 export default defineEventHandler(async (event) => {
 	const { containerId } = await readBody(event);
 	const mutation = getRouterParam(event, "mutate");
 
-	if (!mutation) throw new Error("Missing mutation parameter");
-	if (!containerId) throw new Error("Missing containerId parameter");
-	if (mutation !== "start" && mutation !== "pause" && mutation !== "unpause" && mutation !== "stop") throw new Error("Invalid mutation parameter");
+	if (!mutation) {
+		throw new Error("Missing mutation parameter");
+	}
+	if (!containerId) {
+		throw new Error("Missing containerId parameter");
+	}
+	if (
+		mutation !== "start" &&
+		mutation !== "pause" &&
+		mutation !== "unpause" &&
+		mutation !== "stop"
+	) {
+		throw new Error("Invalid mutation parameter");
+	}
 
 	const docker = new Docker();
 	const container = docker.getContainer(containerId);
 
-	const mutationHandlers: Record<DockerContainerMutations, () => Promise<void>> = {
+	const mutationHandlers: Record<
+		DockerContainerMutations,
+		() => Promise<void>
+	> = {
 		start: async () => {
 			await container.start();
 		},
@@ -25,7 +39,7 @@ export default defineEventHandler(async (event) => {
 		},
 		stop: async () => {
 			await container.stop();
-		}
+		},
 	};
 
 	await mutationHandlers[mutation]();
