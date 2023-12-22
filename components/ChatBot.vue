94dd3ed953ea965430d17ad3a3cf74a38cd2fca0
francismiko <1485059943@qq.com>
2023-12-22 15:09:30 +0800

    chore: style

--- components/ChatBot.vue
+++ components/ChatBot.vue
@@ -8,6 +8,7 @@ const { width } = useWindowSize()
 
 const drawer = ref<boolean>(false)
 const inputText = ref<string>('')
+const question = ref<string>('如何备份和恢复Docker容器和镜像?')
 
 const templateQuestions: string[] = [
   '如何解决Docker容器之间网络通信的问题?',
@@ -57,13 +58,19 @@ const handleChat = async () => {
 
   <el-drawer :size="width < 600 ? '100%' : 500" v-model="drawer" title="OpenAI 驱动的 Chat Bot">
     <div class="h-full">
-      <main class="h-4/5 w-full">Main</main>
+      <main class="h-4/5 w-full px-2 pb-8">
+        <div class="rounded-xl h-full w-full bg-slate-100">
+          <el-scrollbar height="200px">
+            <p class="text-center">{{ question }}</p>
+          </el-scrollbar>
+        </div>
+      </main>
       <footer class="h-1/5 w-full">
         <div :class="'grid grid-cols-2 grid-rows-3 gap-4 h-full'">
-          <div v-for="question in templateQuestions"
-            class="flex text-xs items-center cursor-pointer justify-center px-2 outline rounded outline-slate-300 hover:outline-slate-500 hover:bg-gray-100"
-            @click="() => { inputText = question }">
-            {{ question }}
+          <div v-for="templateQuestion in templateQuestions"
+            class="flex text-xs items-center cursor-pointer justify-center px-2 outline rounded outline-slate-300 hover:outline-slate-500 hover:bg-gray-100 hover:underline hover:underline-offset-2 hover:decoration-dotted"
+            @click="() => { inputText = templateQuestion }">
+            {{ templateQuestion }}
           </div>
           <div class="col-span-2 row-span-1 h-full">
             <el-input v-model="inputText" size="large" autosize placeholder="输入你想问的问题..." @keyup.enter="handleChat">
