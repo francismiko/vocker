71712a6d475f0a2ce396f8e52d019887222ea5a8
francismiko <1485059943@qq.com>
2023-12-22 23:28:20 +0800

    fix: CI check

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,11 +5,12 @@ export default defineNuxtConfig({
 				{
 					rel: "stylesheet",
 					href: "https://cdn.jsdelivr.net/npm/katex@0.16.8/dist/katex.min.css",
-					integrity: "sha384-GvrOXuhMATgEsSwCs4smul74iXGOixntILdUW9XmUC6+HX0sLNAK3q71HotJqlAn",
+					integrity:
+						"sha384-GvrOXuhMATgEsSwCs4smul74iXGOixntILdUW9XmUC6+HX0sLNAK3q71HotJqlAn",
 					crossorigin: "anonymous",
 				},
 			],
-		}
+		},
 	},
 	typescript: {
 		shim: false,
