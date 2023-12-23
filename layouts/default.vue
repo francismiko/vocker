9c161871aea12730a9e53435c8464b283e731334
francismiko <1485059943@qq.com>
2023-12-23 17:47:55 +0800

    fix: router path

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,7 +10,7 @@
       <el-main>
         <el-scrollbar height="80vh">
           <slot />
-          <Chatbot />
+          <ChatBot />
         </el-scrollbar>
       </el-main>
     </el-container>
